d63c130d66b9517a215ecd705a21608487cc8aaa
apollotong <31773490+apollotong@users.noreply.github.com>
2018-04-28 16:02:05 -0400

    Add files via upload
    change to knn search (k= 4)

--- kd_tree.py
+++ kd_tree.py
@@ -1,11 +1,13 @@
 import kdtree
 import collections
 import time
-#empty = kdtree.create(dimensions = 8)
-empty = kdtree.create(dimensions = 7)
+
+empty = kdtree.create(dimensions=8)
+
+
 class Item(object):
     def __init__(self, _list):
-        self.coords = _list[:len(_list)-1]
+        self.coords = _list[:len(_list) - 1]
         self.name = _list[-1]
 
     def __len__(self):
@@ -18,15 +20,15 @@ class Item(object):
         s = "("
         for i in range(len(self.coords)):
             s += str(self.coords[i])
-            if i != len(self.coords)-1:
+            if i != len(self.coords) - 1:
                 s += ","
 
         s += ")"
         return s
-        #return 'Item({},{},{},{}, {},{},{},{},{})'.format(self.coords[0], self.coords[1],self.coords[2],self.coords[3],
-        #                                                  self.coords[4],self.coords[5],self.coords[6],self.coords[7],self.name)
+
+
 def kdtree(filename):
-#     empty = kdtree.create(dimensions = 8)
+    #     empty = kdtree.create(dimensions = 8)
     with open(filename) as f:
         content = f.readlines()
         content = [x.strip('\n') for x in content]
@@ -34,9 +36,9 @@ def kdtree(filename):
         row = row.split(",")
         for i in range(len(row)):
             row[i] = int(row[i])
-        print("!!!! ",row)
-        point = Item( list(row) )
-        print("**** ",point)
+        # print("!!!! ", row)
+        point = Item(list(row))
+        # print("**** ", point)
         empty.add(point)
     return empty
 
@@ -46,20 +48,22 @@ def result(string, empty):
     query = string.split(',')
     for i in range(len(query)):
         _input.append(int(query[i]))
+    # _input.append(4)
 
-    start_time = time.time()
-    result = empty.search_nn(_input)
+    start_time = time.time()   # print(_input)t_time = time.time()
+    result = empty.search_knn(_input,3)
+    print(result)
 
     T = "%s seconds" % (time.time() - start_time)
-    
-    distance = result[-1]
 
-    coordinate = result[0].data
+    distance = result[-1]
+    coordinate = result[0][0].data
 
     str1 = "The nearest neighbor point's coordinate is : " + str(coordinate)
     str2 = "The time cost of kdtree is : " + T
     str3 = "The distance between query point and neareast point is : " + str(distance)
     return [str1, str2, str3]
 
-kdtree('itemset2.txt')
-print(result('1,2,3,4,5,6,7,8',empty))
+
+kdtree('/Users/apollo_tong/PycharmProjects/504_kdtree/new_kdtree/itemset2.txt')
+result('1,2,3,4,5,6,7,8', empty)
